a326781f1bfb8c39cecc88e84d251424b0d56780
ThucNu <thucnu0608@gmail.com>
2022-09-12 20:27:57 +0700

    Xay dung trang cac san pham nguoi dung

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:myshop/ui/products/user_products_screen.dart';
 import 'ui/products/product_detail_screen.dart';
 import 'ui/products/products_manager.dart';
 import 'ui/products/product_overview_screen.dart';
+import 'ui/products/user_products_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +23,9 @@ class MyApp extends StatelessWidget {
               ColorScheme.fromSwatch(primarySwatch: Colors.purple).copyWith(
             secondary: Colors.deepOrange,
           )),
-      home: const SafeArea(child: ProductsOverviewScreen()),
+      home: const SafeArea(
+        child: UserProductsScreen(),
+      ),
     );
   }
 }
