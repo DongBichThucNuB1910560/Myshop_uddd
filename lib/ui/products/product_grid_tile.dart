bcde53114aea5cba0e3f83a4a23a5ea9ff126e7b
ThucNu <thucnu0608@gmail.com>
2022-10-18 09:37:48 +0700

    Cai dat chuc nang chon san pham yeu thich

--- lib/ui/products/product_grid_tile.dart
+++ lib/ui/products/product_grid_tile.dart
@@ -5,26 +5,23 @@ import '../../models/product.dart';
 
 import 'product_detail_screen.dart';
 
-class ProductGridTile extends StatelessWidget{
-  const ProductGridTile( 
-    this.product,{
-      super.key,
-    }
-  );
+class ProductGridTile extends StatelessWidget {
+  const ProductGridTile(
+    this.product, {
+    super.key,
+  });
   final Product product;
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         footer: buildGridFooterBar(context),
         child: GestureDetector(
-          onTap: (){
-            Navigator.of(context).pushNamed(
-              ProductDetailScreen.routeName,
-              arguments: product.id
-            );
+          onTap: () {
+            Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
+                arguments: product.id);
           },
           child: Image.network(
             product.imageUrl,
@@ -34,16 +31,22 @@ class ProductGridTile extends StatelessWidget{
       ),
     );
   }
-  Widget buildGridFooterBar(BuildContext context){
+
+  Widget buildGridFooterBar(BuildContext context) {
     return GridTileBar(
       backgroundColor: Colors.black87,
-      leading: IconButton(
-        icon: Icon(
-          product.isFavorite ? Icons.favorite: Icons.favorite_border,
-        ),
-        color: Theme.of(context).colorScheme.secondary,
-        onPressed: (){
-          print('Toggle a favorite product');
+      leading: ValueListenableBuilder<bool>(
+        valueListenable: product.isFavoriteListenable,
+        builder: (ctx, isFavorite, child) {
+          return IconButton(
+            icon: Icon(
+              product.isFavorite ? Icons.favorite : Icons.favorite_border,
+            ),
+            color: Theme.of(context).colorScheme.secondary,
+            onPressed: () {
+              product.isFavorite = !isFavorite;
+            },
+          );
         },
       ),
       title: Text(
@@ -51,8 +54,8 @@ class ProductGridTile extends StatelessWidget{
         textAlign: TextAlign.center,
       ),
       trailing: IconButton(
-        icon: const Icon( Icons.shopping_cart),
-        onPressed: (){
+        icon: const Icon(Icons.shopping_cart),
+        onPressed: () {
           print('Add item to cart');
         },
         color: Theme.of(context).colorScheme.secondary,
